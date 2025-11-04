1afdd5e1c64773f5489dfcce9c80fef724826cfa
abhishek-031-k <abhishkumar159@gmail.com>
2025-11-05 00:59:03 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ import orderRouter from "./routes/orderRoute.js"
 
 //app config
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 4000
 
 //mmiddleware
 
@@ -35,5 +35,6 @@ app.get("/",(req,res)=>{
 app.listen(port, ()=>{
     console.log(`Server Started on http://localhost:${port}`)
 })
+
 
 
