0805ad7b0622a088253f55275c826507b8327c0c
abhishek-031-k <abhishkumar159@gmail.com>
2025-12-15 21:25:20 +0530

    Create cloudinaryConfig.js

---
+++ Backend/config/cloudinaryConfig.js
@@ -0,0 +1,9 @@
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: "dbdxorxby",
+  api_key: "668913992671833",
+  api_secret: "7erRQ_Fx7wkB2TDWFZ5P9Ar6khI"
+});
+
+export default cloudinary;
